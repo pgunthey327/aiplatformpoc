b214f84174992ea49fde697e9ca336dd2202319f
pgunthey327 <meshprath@gmail.com>
2026-06-16 23:01:10 +0530

    test

--- src/lib/adaptors/langfuse-adaptors.js
+++ src/lib/adaptors/langfuse-adaptors.js
@@ -1,34 +1,18 @@
-import { Langfuse } from "langfuse";
+const { LANGFUSE_PUBLIC_KEY, LANGFUSE_SECRET_KEY, LANGFUSE_HOST } = require("@/constants");
+const { Langfuse } = require("langfuse");
 
 const langfuse = new Langfuse({
-  publicKey: process.env.LANGFUSE_PUBLIC_KEY,
-  secretKey: process.env.LANGFUSE_SECRET_KEY,
-  baseUrl: process.env.LANGFUSE_HOST, // only if self-hosted
+    publicKey: LANGFUSE_PUBLIC_KEY,
+    secretKey: LANGFUSE_SECRET_KEY,
+    baseUrl: LANGFUSE_HOST, // cloud or local
 });
 
-export const getAllTraces = async (agentName, agentId, version) => {
-  const tags = [];
-
-  if (agentName) tags.push(`agent:${agentName}`);
-  if (agentId) tags.push(`id:${agentId}`);
-  if (version) tags.push(`version:${version}`);
-
-  const query = {
-    limit: 100,
-  };
-
-  if (tags.length > 0) {
-    query.tags = tags;
-  }
-
-  try {
-     const result = await langfuse.api.trace.list(query);
-    return result;
-  } catch (err) {
-    console.error("Langfuse error:", err);
-    throw err;
-  }
-};
+export const getAllTraces = async () => {
+    const traces = await langfuse.fetchTraces({
+        limit: 100,
+    });
+    return traces;
+}
 
 export const getTraceByObservationId = async (obsId) => {
     try {
